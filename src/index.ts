4e166a9487fd9059c5d18c42003fe6bda8241a2c
Marco Zimmermann <fuzzyslash@hotmail.com>
2025-06-27 19:23:24 -0600

    Food card

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
 import fluidScale from 'fluid-scale';
 
 fluidScale ();
+
+const pages = ['index.html', 'food-card.html'];
+
+const path = window.location.pathname;
+const fileName = path.substring(path.lastIndexOf('/') + 1);
+
+let currPage = pages.indexOf (fileName);
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'ArrowLeft') {
+      currPage--;
+      if(currPage < 0)
+        currPage = pages.length - 1;
+    } else if (event.key === 'ArrowRight') {
+      currPage++;
+      if(currPage >= pages.length)
+        currPage = 0;
+    }
+
+    window.location.href = pages[currPage];
+  });
